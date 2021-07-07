b3eb778d205ff96a2ae50a1f1f7c626adaf62b04
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-07-07 15:43:25 -0400

    feat: fallback to prop name

--- src/index.ts
+++ src/index.ts
@@ -95,7 +95,7 @@ const mapPropsData = (data: StencilJsonDocsProp[]): ArgTypes => {
     data &&
     data.reduce((acc, item) => {
       acc[item.name] = {
-        name: item.attr,
+        name: item.attr ? item.attr : item.name,
         description: item.docs,
         type: { name: item.type, required: item.required },
         control: mapPropTypeToControl(item),
